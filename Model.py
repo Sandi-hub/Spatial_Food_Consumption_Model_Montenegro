b0133c814533c78c0fecd4bd0bdda642bffe6b60
srude <sandra.rudeloff@stud.the-klu.org>
2021-06-09 21:41:31 +0200

    set Workspace

--- Model.py
+++ Model.py
@@ -1,25 +1,15 @@
 import arcpy
 
-# Set the Workspace of the ArcGIS project
-arcpy.env.workspace = "C:/Users/admin/Documents/ArcGIS/Projects/MyProject/MyProject.gdb"
+# Set the Workspace of the ArcGIS project (Options > Current Setting > Default geodatabase)
+arcpy.env.workspace = r'C:\Users\srude\Documents\ArcGIS\Packages\MyProject_970f65\p20\myproject.gdb'
 
 # set the array of supermarkets (names of the layers)
 supermarkets = ["HDL", "Franca_Geocoded", "Aroma_Geocoded", "Voli_Geocoded1", "Idea_Geocoded"]
-
-
-def search_distances():
-    field = "NEAR_DIST"
-
-    for supermarket in supermarkets:
-        output_table = "C:/Users/admin/Documents/ArcGIS/Projects/MyProject/MyProject.gdb/Output_" + supermarket
-        with arcpy.da.SearchCursor(output_table, field) as cursor:
-            for row in cursor:
-                print(supermarket + " " + str(row[0]))
+# set the outbreak layer
+outbreak = "locate_feature_class_2"
 
 
 def calculate_closest():
-    # set the outbreak layer
-    in_features = "locate_feature_class_2"
 
     # set optional parameters
     search_radius = "100000 Meters"
@@ -33,5 +23,17 @@ def calculate_closest():
     for supermarket in supermarkets:
         out_table = "Output_" + supermarket
         arcpy.GenerateNearTable_analysis(
-            in_features, supermarket, out_table, search_radius, location, angle, closest, closest_count, method
+            outbreak, supermarket, out_table, search_radius, location, angle, closest, closest_count, method
         )
+
+
+def search_distances():
+    field = "NEAR_DIST"
+
+    for supermarket in supermarkets:
+        output_table = "C:/Users/admin/Documents/ArcGIS/Projects/MyProject/MyProject.gdb/Output_" + supermarket
+        with arcpy.da.SearchCursor(output_table, field) as cursor:
+            for row in cursor:
+                print(supermarket + " " + str(row[0]))
+
+calculate_closest()
